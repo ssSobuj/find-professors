0ad10f112cd69bc753eab9f30295e87afb55562c
sobujmia5858 <sabuj.mia@acotegroup.com>
2025-05-24 23:15:54 +0600

    fix deployment error

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,6 @@
-import Navbar from "@/components/Navbar";
 import React from "react";
-import { Footer } from "react-day-picker";
+import Navbar from "@/components/Navbar";
+import Footer from "@/components/Footer";
 
 export default function MainLayout({
   children,
